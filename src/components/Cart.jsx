7753f1c09c0d421abd5e8803c8aef2104fbc909f
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-07-10 18:44:40 -0500

    [FINISH ALL]

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,20 +2,25 @@ import { Fragment } from 'react';
 
 import Prueba from './contenido.json';
 
-
 export default function Example() {
 	return (
 		<>
-				{Prueba.map((item) => (
-                    <div class="min-h-[11rem] w-[90%] sm:w-[60%] bg-[#282623] rounded drop-shadow-xl mb-4" key={item.key}>
-					<div >
-                    <img class="size-8 mt-4 ml-4" src={item.src} alt={item.alt}/>
-                    <p class="font-bold text-[1.4rem] leading-[1.4rem] text-white text-pretty ml-5 mt-3">{item.title}</p>
-                    <p class="font-medium text-[#808080] h-2 w-[90%] text-[1.05rem] leading-[1.1rem] text-pretty ml-5 mt-4">{item.content}</p>
-                </div>
-
-                    </div>
-				))}
+			{Prueba.map((item) => (
+				<div
+					class="mb-4 min-h-[11rem] w-[90%] rounded bg-[#282623] drop-shadow-xl sm:w-[60%]"
+					key={item.key}
+				>
+					<div>
+						<img class="ml-4 mt-4 size-8" src={item.src} alt={item.alt} />
+						<p class="ml-5 mt-3 text-pretty text-[1.4rem] font-bold leading-[1.4rem] text-white">
+							{item.title}
+						</p>
+						<p class="ml-5 mt-4 h-2 w-[90%] text-pretty text-[1.05rem] font-medium leading-[1.1rem] text-[#808080]">
+							{item.content}
+						</p>
+					</div>
+				</div>
+			))}
 		</>
 	);
 }
